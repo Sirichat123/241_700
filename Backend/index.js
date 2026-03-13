34f166989560009bcb447417f1a1d4172174dbc7
natchanonkaewn <natchanon.kaewn@ku.th>
2026-03-13 08:45:08 +0700

    test

--- Backend/index.js
+++ Backend/index.js
@@ -1,24 +1,71 @@
+/*
+//ทำการ import โมดูล http
+const http = require('http');
+const host = 'localhost';
+const port = 8000;
+
+//กำหนดค่าเซิร์ฟเวอร์
+
+const requestListener = function (req, res) {
+    res.writeHead(200);
+    res.end('Hello, World! This is my first server.');
+}
+//run server
+const server = http.createServer(requestListener);
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
+});
+*/
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const e = require('express');
+const { use } = require('react');
 const mysql = require('mysql2/promise');
 const app = express();
-const port = 8000;
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(bodyParser.json());
 
-let users = []
-let counter = 1;
-let conn = null
+const port = 8000;
 
+let conn = null;
 const initMySQL = async () => {
     conn = await mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'root',
         database: 'webdb',
-        port: 8820
+        port: 8700
     });
+    console.log('Connected to MySQL database');
 }
+
+let users = [];
+let counter = 1;
+
+/*
+app.get('/testdb', (req, res) => {
+    mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'root',
+        database: 'webdb',
+        port: 8700
+    }).then((conn) => {
+        conn
+        .query('SELECT * FROM users')
+        .then((results) => {
+            res.json(results[0]);
+        }).catch((err) => {
+            console.error('Error executing query:', err);
+            res.json({ error: err});
+        });
+    })
+});
+*/
 
 //path = GET /users สำหรับด get ข้อมูล users ทั้งหมด
 app.get('/users', async (req, res) => {
@@ -97,7 +144,7 @@ app.delete('/users/:id', async (req, res) => {
         const results = await conn.query('DELETE FROM users WHERE id = ?', id)
         if (results[0].affectedRows == 0) {
             throw { statusCode: 404, message: 'User not found' };
-        }   
+        }
         res.json({
             message: 'User deleted successfully'
         });
@@ -112,7 +159,142 @@ app.delete('/users/:id', async (req, res) => {
     }
 })
 
+/*
+app.get('/testdb-new', async (req, res) => {
+    try {
+        const conn = await mysql.createConnection({
+            host: 'localhost',
+            user: 'root',
+            password: 'root',
+            database: 'webdb',
+            port: 8700
+        });
+        const results = await conn.query('SELECT * FROM users');
+        res.json(results[0]);
+    } catch (err) {
+        console.error('Error connecting to database:', err);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+*/
+
+/**
+ * GET /users - ดึงข้อมูลผู้ใช้ทั้งหมด
+ * POST /user - เพิ่มผู้ใช้ใหม่
+ * PUT /user/:id - แก้ไขข้อมูลผู้ใช้ตาม id (แก้ไขทั้งหมด
+ * PATCH /user/:id - แก้ไขข้อมูลผู้ใช้ตาม id (แก้ไขบางส่วน)
+ * DELETE /user/:id - ลบผู้ใช้ตาม id
+*/
+
+//path: /test
+app.get('/test', (req, res) => {
+
+    let user = {
+        name: 'John Doe',
+        age: 30,
+        email: 'john.doe@example.com'
+    };
+
+    //res.send('Hello, World! test');
+    res.json(user);
+
+});
+
+
+//path: GET /users
+/*
+app.get('/users', (req, res) => {
+    res.json(users);
+});
+*/
+
+
+//path: POST /user
+app.post('/user', (req, res) => {
+    //res.json({message: 'Data received successfully'});
+    let user = req.body;
+    users.push(user);
+    user.id = counter;
+    counter += 1;
+
+    res.json({
+        message: 'User added successfully',
+        user: user
+    });
+    //res.send(req.body);
+});
+
+
+//path: PUT /user/:id
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+
+    //หา user จาก id ที่ส่งมา
+    let selectIndex = users.findIndex(user => user.id == id);
+    //res.send(selectindex+'');
+
+    //อัพเดตข้อมูล user ตาม id ที่ส่งมา
+    users[selectIndex].firstname = updatedUser.firstname || users[selectIndex].firstname;
+    users[selectIndex].lastname = updatedUser.lastname || users[selectIndex].lastname;
+
+    res.json({
+        message: 'User updated successfully',
+        data: {
+            user: updatedUser,
+            indexUpdate: selectIndex
+        }
+    });
+
+    //ส่ง users ที่อัพเดทแล้วกลับไป
+
+});
+
+app.patch('/user/:id', (req, res) => {
+    let id = req.params.id;
+    let updatedUser = req.body;
+
+    //หา user จาก id ที่ส่งมา
+    let selectIndex = users.findIndex(user => user.id == id);
+    //res.send(selectindex+'');
+
+    //อัพเดตข้อมูล user ตาม id ที่ส่งมา
+    if (updatedUser.firstname) {
+        users[selectIndex].firstname = updatedUser.firstname;
+    }
+    if (updatedUser.lastname) {
+        users[selectIndex].lastname = updatedUser.lastname;
+    }
+
+    res.json({
+        message: 'User updated successfully',
+        data: {
+            user: users[selectIndex],
+            indexUpdate: selectIndex
+        }
+    });
+
+    //ส่ง users ที่อัพเดทแล้วกลับไป
+
+});
+
+app.delete('/user/:id', (req, res) => {
+    let id = req.params.id;
+
+    //หา id ที่ต้องการลบ
+    let selectIndex = users.findIndex(user => user.id == id);
+
+
+    //ลบ user ตาม id ที่ส่งมา
+    users.splice(selectIndex, 1);
+
+    res.json({
+        message: 'User deleted successfully',
+        indexDeleted: selectIndex
+    });
+});
+
 app.listen(port, async () => {
     await initMySQL();
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on http://localhost:${port}`);
 });
